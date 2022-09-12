cea3e99e69ad46f2e52021e88e01acb7e9691cf3
Nathasha <nsoares@corpcloud.com.au>
2022-09-12 15:52:02 +0800

    buttons css

--- src/app.js
+++ src/app.js
@@ -30,48 +30,48 @@ function formatDay(timestamp) {
   return days[day];
 }
 
-function displayForecast(response) {
-  let forecast = response.data.daily;
+// function displayForecast(response) {
+//   let forecast = response.data.daily;
 
-  let forecastElement = document.querySelector("#forecast");
+//   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        `
-      <div class="col-2">
-        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-        <img
-          src="http://openweathermap.org/img/wn/${
-            forecastDay.weather[0].icon
-          }@2x.png"
-          alt=""
-          width="42"
-        />
-        <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${Math.round(
-            forecastDay.temp.max
-          )}° </span>
-          <span class="weather-forecast-temperature-min"> ${Math.round(
-            forecastDay.temp.min
-          )}° </span>
-        </div>
-      </div>
-  `;
-    }
-  });
+//   let forecastHTML = `<div class="row">`;
+//   forecast.forEach(function (forecastDay, index) {
+//     if (index < 6) {
+//       forecastHTML =
+//         forecastHTML +
+//         `
+//       <div class="col-2">
+//         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+//         <img
+//           src="http://openweathermap.org/img/wn/${
+//             forecastDay.weather[0].icon
+//           }@2x.png"
+//           alt=""
+//           width="42"
+//         />
+//         <div class="weather-forecast-temperatures">
+//           <span class="weather-forecast-temperature-max"> ${Math.round(
+//             forecastDay.temp.max
+//           )}° </span>
+//           <span class="weather-forecast-temperature-min"> ${Math.round(
+//             forecastDay.temp.min
+//           )}° </span>
+//         </div>
+//       </div>
+//   `;
+//     }
+//   });
 
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
-}
+//   forecastHTML = forecastHTML + `</div>`;
+//   forecastElement.innerHTML = forecastHTML;
+// }
 
-function getForecast(coordinates) {
-  let apiKey = "d122489789ce9e01ba81bb0f4a64028b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
-}
+// function getForecast(coordinates) {
+//   let apiKey = "d122489789ce9e01ba81bb0f4a64028b";
+//   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+//   axios.get(apiUrl).then(displayForecast);
+// }
 
 function displayWeather(response) {
   let cityElement = document.querySelector("#city");
@@ -97,7 +97,7 @@ function displayWeather(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
-  getForecast(response.data.coord);
+  // getForecast(response.data.coord);
 }
 
 
